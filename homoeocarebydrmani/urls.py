11c9e99b5c2037d716880ff9e333048b02d204b9
Saiprasad7649 <Sgone@govst.edu>
2026-09-19 07:52:01 -0500

    Add sitemap configuration and apps

--- homoeocarebydrmani/urls.py
+++ homoeocarebydrmani/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
+
+# Basic sitemap setup
+sitemaps = {}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 # This block enables serving uploaded media files during development (DEBUG = True)
